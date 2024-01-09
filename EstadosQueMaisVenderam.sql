be436d24eef4f6f08bc5ed0b91f72936757defec
MateusMalvezzi <mateusmalvezzir@hotmail.com>
2024-01-09 08:38:30 -0300

    fix code - uppercase

--- EstadosQueMaisVenderam.sql
+++ EstadosQueMaisVenderam.sql
@@ -1,15 +1,15 @@
 -- Estados que mais venderam
 -- Colunas: país, estado, vendas (#)
 
-select
-	'Brazil' as país,
-	cus.state as estado,
-	count(fun.paid_date) as "vendas (#)"
+SELECT
+	'Brazil' AS país
+	,cus.state AS estado
+	,count(fun.paid_date) AS "vendas (#)"
 
-from sales.funnel as fun
-left join sales.customers as cus
-	on fun.customer_id = cus.customer_id
-where paid_date between '2021-08-01' and '2021-08-31'
-group by país, estado
-order by "vendas (#)" desc
-limit 5
+FROM sales.funnel AS fun
+LEFT JOIN sales.customers AS cus
+	ON fun.customer_id = cus.customer_id
+WHERE paid_date BETWEEN '2021-08-01' AND '2021-08-31'
+GROUP BY país, estado
+ORDER BY "vendas (#)" DESC
+LIMIT 5
